b5e72514ae9280152e908fa2a13186ba36f504cf
arturo51236 <rtsanonynousperson@gmail.com>
2022-05-26 09:29:50 +0200

    sdm660-common: Uprev seom HIDL
    Signed-off-by: Arturo <rtsanonynousperson@gmail.com>
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- sdm660.mk
+++ sdm660.mk
@@ -280,7 +280,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0.vendor
+    android.hardware.keymaster@3.0.vendor
 
 # Lights
 PRODUCT_PACKAGES += \
@@ -323,7 +323,6 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
@@ -354,8 +353,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.1.vendor \
-    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.0 \
     android.hardware.secure_element@1.0.vendor \
